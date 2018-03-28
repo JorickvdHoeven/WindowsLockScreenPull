8d11fa7642bcd63419ff61fe362d5db9f77dc7cc
Jorick van der Hoeven <jorick@vanderhoeven.ch>
2018-03-28 13:27:16 -0400

    Update to use APIKey file storing the Azure API Key

--- Python_image_loader.py
+++ Python_image_loader.py
@@ -8,14 +8,16 @@ from sys import argv
 from PIL import Image
 from requests import post, HTTPError
 from pandas import DataFrame, concat, read_csv
+import APIKey
 
-#load metadata file with Hashes if file not exist, hash all files in destination first
+
 class Python_image_loader:
+
     def __init__(self, sourceDir, destDir):
         """ Initialise all of the variables and set the API Key
         """
-        #### 1. Initilise, grab variables
-        self.subscription_key = "AZURE API KEY GOES HERE"
+        # 1. Initilise, grab variables
+        self.subscription_key = APIKey.AzureAPIKey
         self.vision_base_url = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/"
         self.vision_analyze_url = self.vision_base_url + "analyze"
 
@@ -23,7 +25,7 @@ class Python_image_loader:
         self.destinationDir = destDir
         self._copyLogFile = self.destinationDir + 'copyLog.dat'
 
-        #### 2. Check for source and Destination folders (if no Dest, create dest. If not source, fail)
+        # 2. Check for source and Destination folders (if no Dest, create dest. If not source, fail)
         if not path.exists(self.sourceDir):
             exit()
 
@@ -31,11 +33,11 @@ class Python_image_loader:
             print('[+] Destination directory doesn''t exist, creating...')
             makedirs(self.destinationDir)
 
-        #### 3. Check for source log (If no source log, generate)
+        # 3. Check for source log (If no source log, generate)
         if not path.exists(self._copyLogFile):
             self.__reset_copy_log()
         else:
-            #### 4. Load source log data into memory
+            # 4. Load source log data into memory
             self.__load_copy_log(self._copyLogFile)
 
     def __reset_copy_log(self):
@@ -52,9 +54,9 @@ class Python_image_loader:
         with open(logfile, 'r') as f:
             self._copyLog = read_csv(f, quoting=1)
         print('[!!] TODO: Perform Copy Log integrity Checks')
-        #TODO: check that all of the files still exist, and if not find them
+        # TODO: check that all of the files still exist, and if not find them
 
-    #Hash all files in source
+    # Hash all files in source
     def get_hashes(self, source, no_jpg_size_filter=True):
         """ Returns SHA256 hashes of files in the folder. 
             Setting True to no_jpg_size_filter allows the function 
@@ -74,23 +76,25 @@ class Python_image_loader:
                                 if not data:
                                     break
                                 sha1.update(data)
-                            line = DataFrame([{"SHA256":sha1.hexdigest(), "Path":path.join(root, name), "Azure_Processed":0}])
-                            hash_dict = concat([hash_dict, line], axis=0, ignore_index=True)
-        #print(hash_dict.to_string())
+                            line = DataFrame([{"SHA256": sha1.hexdigest(), "Path": path.join(
+                                root, name), "Azure_Processed": 0}])
+                            hash_dict = concat(
+                                [hash_dict, line], axis=0, ignore_index=True)
+        # print(hash_dict.to_string())
         return hash_dict
 
     def azureVisionUpdate(self, image_path):
         """ This is a wrapper for the Azure Compute Vision API call
         """
         image_data = open(image_path, "rb").read()
-        headers    = {'Ocp-Apim-Subscription-Key': self.subscription_key,
-                      "Content-Type": "application/octet-stream"}
-        params     = {'visualFeatures': 'Categories,Description,Color'}
+        headers = {'Ocp-Apim-Subscription-Key': self.subscription_key,
+                   "Content-Type": "application/octet-stream"}
+        params = {'visualFeatures': 'Categories,Description,Color'}
         try:
             response = post(self.vision_analyze_url,
-                                     headers=headers,
-                                     params=params,
-                                     data=image_data)
+                            headers=headers,
+                            params=params,
+                            data=image_data)
             response.raise_for_status()
             analysis = response.json()
         except HTTPError as e:
@@ -99,26 +103,29 @@ class Python_image_loader:
             self._copyLog.to_csv(self._copyLogFile, quoting=1)
             exit()
 
-        #print(analysis)
-        image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+        # print(analysis)
+        image_caption = analysis["description"]["captions"][0]["text"].capitalize(
+        )
         main_category = analysis["categories"]
         if len(main_category) > 0:
-            prefix = max(analysis["categories"], key=lambda x: x['score'])['name']
+            prefix = max(analysis["categories"],
+                         key=lambda x: x['score'])['name']
         else:
             prefix = 'NoCat'
-        return prefix.upper()+'_'+image_caption.replace(' ', '_')+'.jpg'
+        return prefix.upper() + '_' + image_caption.replace(' ', '_') + '.jpg'
 
     def __unprocessedFileName(self, filename):
         if len(filename) == 68 and '_' not in filename:
             return True
-        else: 
+        else:
             return False
 
     def findPics(self):
-        #get the source Hashes
-        print('[+] Examining new files and checking for viable backgrounds (jpg, 1920x1080 only)')
+        # get the source Hashes
+        print(
+            '[+] Examining new files and checking for viable backgrounds (jpg, 1920x1080 only)')
         newHashes = self.get_hashes(self.sourceDir, False)
-        #test for new ones
+        # test for new ones
         newHashes = newHashes.merge(self._copyLog, on=['SHA256'],
                                     how='left', indicator=True)
 
@@ -126,22 +133,26 @@ class Python_image_loader:
 
         if newHashes.shape[0] > 0:
             print('[+] Copying {} files over'.format(newHashes.shape[0]))
-            #move new ones to destination
+            # move new ones to destination
             for index, row in newHashes.iterrows():
-                copyfile(row['Path_x'], path.join(self.destinationDir, path.basename(row['Path_x']+'.jpg')))
-                line = DataFrame([{"SHA256":row['SHA256'], "Path":path.join(self.destinationDir, path.basename(row['Path_x']+'.jpg')), "Azure_Processed":0}])
-                self._copyLog = concat([self._copyLog, line], axis=0, ignore_index=True)
-            
-            #update the copyLog file given that we've updated the files
+                copyfile(row['Path_x'], path.join(
+                    self.destinationDir, path.basename(row['Path_x'] + '.jpg')))
+                line = DataFrame([{"SHA256": row['SHA256'], "Path":path.join(
+                    self.destinationDir, path.basename(row['Path_x'] + '.jpg')), "Azure_Processed":0}])
+                self._copyLog = concat(
+                    [self._copyLog, line], axis=0, ignore_index=True)
+
+            # update the copyLog file given that we've updated the files
             self._copyLog.to_csv(self._copyLogFile, quoting=1)
         else:
             print('[+] No new files found')
 
         print('[+] Checking files for taggable files')
-        #check jpeg files for wierd 65 char file names and update
+        # check jpeg files for wierd 65 char file names and update
         for index, row in self._copyLog.loc[self._copyLog['Azure_Processed'] == 0].iterrows():
             if self.__unprocessedFileName(path.basename(row['Path'])):
-                print('[+] Starting Azure image tagging for {} ...'.format(path.basename(row['Path'])))
+                print(
+                    '[+] Starting Azure image tagging for {} ...'.format(path.basename(row['Path'])))
                 endTime = datetime.now() + timedelta(seconds=3)
                 newFileName = self.azureVisionUpdate(row['Path'])
                 newFileName = newFileName.replace('__', '_')
@@ -149,32 +160,36 @@ class Python_image_loader:
                     sleep(0.5)
                 newPath = path.join(path.dirname(row['Path']), newFileName)
                 if path.isfile(newPath):
-                    newPath = newPath[:-4] +' (1)'+newPath[-4:]
+                    newPath = newPath[:-4] + ' (1)' + newPath[-4:]
                 try:
                     rename(row['Path'], newPath)
                     self._copyLog.at[index, 'Path'] = newPath
                     self._copyLog.at[index, 'Azure_Processed'] = 1
-                    print('[+] Azure image tagging finished, new name: {}'.format(newFileName))
+                    print(
+                        '[+] Azure image tagging finished, new name: {}'.format(newFileName))
                 except OSError:
-                    print('[ERROR] Image {} not updated'.format(path.basename(row['Path'])))
+                    print('[ERROR] Image {} not updated'.format(
+                        path.basename(row['Path'])))
         self._copyLog.to_csv(self._copyLogFile, quoting=1)
         print('[+] Copy Log Saved')
 
 
 def main():
     if len(argv) < 2:
-        #This may change between machines.. will need to be tested
-        sourceDir = getenv('LOCALAPPDATA')+"\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\"
-        destinationDir = getenv('HOMEPATH')+"\\Pictures\\Windows Spotlight\\"
+        # This may change between machines.. will need to be tested
+        sourceDir = getenv(
+            'LOCALAPPDATA') + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\"
+        destinationDir = getenv('HOMEPATH') + "\\Pictures\\Windows Spotlight\\"
     else:
         if len(argv) == 2:
             sourceDir = argv[0]
             destinationDir = argv[0]
-            if not path.isdir(sourceDir) :
+            if not path.isdir(sourceDir):
                 print('[ERROR] Source directory doesn''t exist')
                 exit()
         else:
-            print('[ERROR] wrong arguments given, format is: PythonImageLoader.py Source Destination')
+            print(
+                '[ERROR] wrong arguments given, format is: PythonImageLoader.py Source Destination')
             exit()
 
     pil = Python_image_loader(sourceDir, destinationDir)
@@ -183,6 +198,5 @@ def main():
     exit()
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
